106f487e84fee4e495baa5fbc6a8fe8a5f998475
Sandra Sanchez <sandra.sanchez.paez@uni-potsdam.de>
2022-08-09 12:16:10 +0200

    Adapt tests to all classes. All work but test_visualize_graph, need to find the way to compare actual and expected

--- tests.py
+++ tests.py
@@ -4,32 +4,7 @@
 # Datum: 31.07.2022
 
 
-from bktree import BKTree, create_triple, get_edge_labels
-from bktree import calculate_levenshtein_distance, calculate_hamming_distance
-
-
-class TestLevenshtein:
-
-    def test_distance_0(self):
-        assert calculate_levenshtein_distance('man', 'man') == 0
-
-    def test_distance_empty_string(self):
-        assert calculate_levenshtein_distance('', 'man') == 3
-
-    def test_different_length_words(self):
-        assert calculate_levenshtein_distance('help', 'loop') == 3
-
-
-class TestHamming:
-
-    def test_different_length(self):
-        assert True == calculate_hamming_distance('book', 'books')
-
-    def test_same_length_one(self):
-        assert calculate_hamming_distance('can', 'man') == 1
-
-    def test_same_legth_many(self):
-        assert calculate_hamming_distance('miracle', 'milagro') == 4
+from bktree import BKTree, Graph
 
 
 class TestBKTree:
@@ -40,37 +15,65 @@ class TestBKTree:
     def test_search_word(self):
         assert self.tree.search_word('book', 1) == ['book', 'books', 'boo', 'boon', 'cook']
 
+    # class TestLevenshtein:
+    def test_distance_0(self):
+        assert self.tree.calculate_levenshtein_distance('man', 'man') == 0
 
-def test_create_triple():
-    connections = ('book', {1: ('books', {2: ('boo', {1: ('boon', {}), 2: ('cook', {})})}),
-                            4: ('cake', {0: ('cake', {}), 1: ('cape', {}), 2: ('cart', {})})})
-    assert create_triple(connections) == [
-        ('book', 'books', 1), ('books', 'boo', 2), ('boo', 'boon', 1), ('boo', 'cook', 2), ('book', 'cake', 4),
-        ('cake', 'cake', 0), ('cake', 'cape', 1), ('cake', 'cart', 2)
-    ]
+    def test_distance_empty_string(self):
+        assert self.tree.calculate_levenshtein_distance('', 'man') == 3
+
+    def test_different_length_words(self):
+        assert self.tree.calculate_levenshtein_distance('help', 'loop') == 3
+
+    # class TestHamming:
+
+    def test_different_length(self):
+        assert True == self.tree.calculate_hamming_distance('book', 'books')
+
+    def test_same_length_one(self):
+        assert self.tree.calculate_hamming_distance('can', 'man') == 1
+
+    def test_same_length_many(self):
+        assert self.tree.calculate_hamming_distance('miracle', 'milagro') == 4
 
 
-def test_visualize_graph():
-    test_triples = [
-        ('book', 'books', 1), ('books', 'boo', 2), ('boo', 'boon', 1),
-        ('boo', 'cook', 2), ('book', 'cake', 4), ('cake', 'cake', 0),
-        ('cake', 'cape', 1), ('cake', 'cart', 2)
-    ]
-    test_tuples = [
-        ('book', 'books'), ('books', 'boo'), ('boo', 'boon'), ('boo', 'cook'),
-        ('book', 'cake'), ('cake', 'cake'), ('cake', 'cape'), ('cake', 'cart')
-    ]
-    assert visualize_graph(test_tuples, test_triples) == 0
+class TestGraph:
+    words = ["book", "books", "cake", "boo", "boon", "cook", "cake", "cape", "cart"]
+    tree = BKTree(words)
+    tree.build_tree()
+    print(tree)
+    graph = Graph(tree.tree)
+    graph.create_triples()
+    graph.create_tuples()
 
+    def test_create_triple(self):
+        # tree.tree = ('book', {1: ('books', {2: ('boo', {1: ('boon', {}), 2: ('cook', {})})}),
+        #                         4: ('cake', {0: ('cake', {}), 1: ('cape', {}), 2: ('cart', {})})})
+        assert self.graph.create_triples() == [
+            ('book', 'books', 1), ('books', 'boo', 2), ('boo', 'boon', 1), ('boo', 'cook', 2), ('book', 'cake', 4),
+            ('cake', 'cake', 0), ('cake', 'cape', 1), ('cake', 'cart', 2)
+        ]
 
-def test_get_edge_labels():
-    test_triples = [
-        ('book', 'books', 1), ('books', 'boo', 2), ('boo', 'boon', 1),
-        ('boo', 'cook', 2), ('book', 'cake', 4), ('cake', 'cake', 0),
-        ('cake', 'cape', 1), ('cake', 'cart', 2)
-    ]
-    assert get_edge_labels(test_triples) == {
-        ('book', 'books'): 1, ('books', 'boo'): 2, ('boo', 'boon'): 1,
-        ('boo', 'cook'): 2, ('book', 'cake'): 4, ('cake', 'cake'): 0,
-        ('cake', 'cape'): 1, ('cake', 'cart'): 2
-    }
+    def test_visualize_graph(self):
+        test_triples = [
+            ('book', 'books', 1), ('books', 'boo', 2), ('boo', 'boon', 1),
+            ('boo', 'cook', 2), ('book', 'cake', 4), ('cake', 'cake', 0),
+            ('cake', 'cape', 1), ('cake', 'cart', 2)
+        ]
+        test_tuples = [
+            ('book', 'books'), ('books', 'boo'), ('boo', 'boon'), ('boo', 'cook'),
+            ('book', 'cake'), ('cake', 'cake'), ('cake', 'cape'), ('cake', 'cart')
+        ]
+        assert self.graph.visualize_graph() == 0
+
+    def test_get_edge_labels(self):
+        test_triples = [
+            ('book', 'books', 1), ('books', 'boo', 2), ('boo', 'boon', 1),
+            ('boo', 'cook', 2), ('book', 'cake', 4), ('cake', 'cake', 0),
+            ('cake', 'cape', 1), ('cake', 'cart', 2)
+        ]
+        assert self.graph.get_edge_labels() == {
+            ('book', 'books'): 1, ('books', 'boo'): 2, ('boo', 'boon'): 1,
+            ('boo', 'cook'): 2, ('book', 'cake'): 4, ('cake', 'cake'): 0,
+            ('cake', 'cape'): 1, ('cake', 'cart'): 2
+        }
