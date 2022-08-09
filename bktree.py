bc206d7b79690cee5e12c7f4204ddbb3f67e9ca7
Sandra Sanchez <sandra.sanchez.paez@uni-potsdam.de>
2022-08-09 10:03:01 +0200

    Graph class does not work properly because methods Create_triples and create_tuple do not work recursively, since they need a tree object.

--- bktree.py
+++ bktree.py
@@ -18,72 +18,69 @@ class StringMetric:
         self.string_2 = string_2
 
 
-def read_data_from_filename(filename):
-    with open(filename) as file:
-        content = file.read().lower().split()
-        return content
+class Graph:
 
+    def __init__(self, tree):
+        self.tree = tree
+        self.tuples = None
+        self.triples = None
+        self.labels = None
 
-def create_triple(nested_tuple):
-    """
-    Take in a nested tuple with a bk-tree structure and adapt it to
-    a tuple of 3 format, where node_1, node_2 and their edit distance is
-    represented. The resulting list of tuples will be then used by networkx
-    to draw a graph visualization of the bk-tree structure.
-    :param nested_tuple: str, str, int
-    :return: list of labels
-    """
-    triples = []
-    node_1 = nested_tuple[0]
-    nested_element = nested_tuple[1]
-    for distance in nested_element:
-        node_2 = nested_element[distance][0]
-        triple = node_1, node_2, distance
-        triples.append(triple)
-        new_tuple = nested_element[distance]
-        if new_tuple[1]:
-            triples = triples + create_triple(new_tuple)
+    def create_triples(self):
+        """
+        Take in a nested tuple with a bk-tree structure and adapt it to
+        a tuple of 3 format, where node_1, node_2 and their edit distance is
+        represented. The resulting list of tuples will be then used by networkx
+        to draw a graph visualization of the bk-tree structure.
+        :return: list of labels
+        """
+        triples = []
+        node_1 = self.tree[0]
+        nested_element = self.tree[1]
+        for distance in nested_element:
+            node_2 = nested_element[distance][0]
+            triple = node_1, node_2, distance
+            triples.append(triple)
+            new_tuple = nested_element[distance]
+            if new_tuple[1]:
+                self.triples = triples + self.create_triples(new_tuple)
 
-    return triples
+        return self.triples
 
+    def create_tuple(self):
+        tuples = []
+        node_1 = self.tree[0]
+        nested_element = self.tree[1]
+        for distance in nested_element:
+            node_2 = nested_element[distance][0]
+            tuple = node_1, node_2
+            tuples.append(tuple)
+            new_tuple = nested_element[distance]
+            if new_tuple[1]:
+                self.tuples = tuples + self.create_tuple(new_tuple)
 
-def create_tuple(nested_tuple):
-    tuples = []
-    node_1 = nested_tuple[0]
-    nested_element = nested_tuple[1]
-    for distance in nested_element:
-        node_2 = nested_element[distance][0]
-        tuple = node_1, node_2
-        tuples.append(tuple)
-        new_tuple = nested_element[distance]
-        if new_tuple[1]:
-            tuples = tuples + create_tuple(new_tuple)
+        return self.tuples
 
-    return tuples
+    def get_edge_labels(self):
+        self.labels = {}
+        for triple in self.triples:
+            node_1 = triple[0]
+            node_2 = triple[1]
+            label = triple[2]
+            if (node_1, node_2) not in self.labels:
+                self.labels[node_1, node_2] = label
 
+        return self.labels
 
-def visualize_graph(tuples, triples):
-    G = nx.DiGraph()
-    G.add_edges_from(tuples)
-    pos = graphviz_layout(G, prog='dot')
-    # G.add_weighted_edges_from(triples)
-    nx.draw_networkx(G, pos=pos, node_size=1500, arrows=True)
-    edge_labels = get_edge_labels(triples)
-    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
-    plt.savefig("bktree.png")
-    plt.show()
-
-
-def get_edge_labels(triples):
-    labels = {}
-    for triple in triples:
-        node_1 = triple[0]
-        node_2 = triple[1]
-        label = triple[2]
-        if (node_1, node_2) not in labels:
-            labels[node_1, node_2] = label
-
-    return labels
+    def visualize_graph(self):
+        tree_graph = nx.DiGraph()
+        tree_graph.add_edges_from(self.tuples)
+        positions = graphviz_layout(tree_graph, prog='dot')
+        nx.draw_networkx(tree_graph, pos=positions, node_size=1500, arrows=True)
+        edge_labels = self.get_edge_labels()
+        nx.draw_networkx_edge_labels(tree_graph, pos=positions, edge_labels=edge_labels)
+        plt.savefig("bktree.png")
+        plt.show()
 
 
 class BKTree:
@@ -116,7 +113,8 @@ class BKTree:
                         self.calculate_levenshtein_distance(string_1[1:], string_2[1:]) + (string_1[0] != string_2[0]))
         return edits
 
-    def calculate_hamming_distance(self, string_1, string_2):
+    @staticmethod
+    def calculate_hamming_distance(string_1, string_2):
         """
         If two strings are of the same length, calculate the
         number of substitutions to turn one string into the
@@ -176,6 +174,12 @@ class BKTree:
         number_of_words = len(self.wordlist)
 
 
+def read_data_from_filename(filename):
+    with open(filename) as file:
+        content = file.read().lower().split()
+        return content
+
+
 if __name__ == '__main__':
     # First stage: read data from file and build bk tree
     filename = sys.argv[1]
@@ -189,12 +193,13 @@ if __name__ == '__main__':
     print(test_tree.search_word('help', 1))
 
     # Second stage: Visualize bk-tree as graph
+    tree_graph = Graph(built_tree)
     # Reformat data
-    test_triples = create_triple(built_tree)
+    test_triples = tree_graph.create_triples()
     print(test_triples)
-    tests_tuples = create_tuple(built_tree)
+    tests_tuples = tree_graph.create_tuple()
     print(tests_tuples)
-    graph = visualize_graph(tests_tuples, test_triples)
+    graph = tree_graph.visualize_graph()
 
     # Third stage: interactive mode (word query)
     user_input = input('Please enter a word query and the desired edit distance\n')
