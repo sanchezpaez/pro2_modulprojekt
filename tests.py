fc0d0697dec82a47ab9436f99b6a8ba581ccf096
Sandra Sanchez <sandra.sanchez.paez@uni-potsdam.de>
2022-08-19 10:21:40 +0200

    Check and complete all tests.

--- tests.py
+++ tests.py
@@ -9,19 +9,63 @@ from networkx import DiGraph
 from networkx.drawing.nx_pydot import graphviz_layout
 
 from bk_tree import BKTree
+from file import File
 from graph import Graph
 
 words = ["book", "books", "cake", "boo", "boon", "cook", "cape", "cart"]
 
 
+class TestFile:
+    dataset = File('demo_words.txt')
+
+    def test_load_vocab(self):
+        assert self.dataset.load_vocab() == [
+            'help', 'hell', 'hello', 'loop', 'helps', 'troop', 'shell', 'helper'
+        ]
+
+    def test_make_bktree_from_file_type(self):
+        assert type(self.dataset.make_bktree_from_file()) == BKTree
+
+    def test_make_bktree_from_file_tree_unloaded(self):
+        assert self.dataset.make_bktree_from_file().tree == (
+            'help', {1: ('hell', {2: ('helps', {})}),
+                     2: ('hello', {2: ('shell', {}),
+                                   3: ('helper', {})}), 3: ('loop', {}), 4: ('troop', {})}
+        )
+
+    def test_make_bktree_from_file_tree_loaded(self):
+        assert self.dataset.make_bktree_from_file().tree == self.dataset.make_bktree_from_file(is_loaded=True).tree
+
+
 class TestBKTree:
     tree = BKTree(words, 'words')
     built_tree = tree.build_tree()
+    tree_2 = BKTree(words, 'words_2')
 
-    def test_search_word(self):
+    def test_build_loaded_tree(self):
+        assert self.tree_2.build_tree(is_loaded=True) == (
+            'book', {1: ('books', {2: ('boo', {1: ('boon', {}),
+                                               2: ('cook', {})})}),
+                     4: ('cake', {1: ('cape', {}), 2: ('cart', {})})}
+        )
+        assert self.tree.tree == self.tree_2.tree
+
+    def test_calculate_height(self):
+        assert self.tree.calculate_height(self.tree.tree) == 4
+
+
+    # Test search_word
+
+    def test_search_word_no_matches(self):
+        assert self.tree.search_word('hedgehog', 1) == []
+
+    def test_search_word_no_distance(self):
+        assert self.tree.search_word('book', 0) == ['book']
+
+    def test_search_word_several_matches(self):
         assert self.tree.search_word('book', 1) == ['book', 'books', 'boo', 'boon', 'cook']
 
-    # TestLevenshtein:
+    # Test calculate_levenshtein_distance:
     def test_distance_0(self):
         assert self.tree.calculate_levenshtein_distance('man', 'man') == 0
 
@@ -31,12 +75,12 @@ class TestBKTree:
     def test_different_length_words(self):
         assert self.tree.calculate_levenshtein_distance('help', 'loop') == 3
 
-    # TestDamerau:
+    # Test calculate_damerau_levenshtein:
     def test_damerau_levenshtein(self):
         assert self.tree.calculate_damerau_levenshtein('ab', 'ba') == 1
         assert self.tree.calculate_damerau_levenshtein('abcdef', 'abcfad') == 2
 
-    # TestHamming:
+    # Test calculate_hamming_distance:
     def test_different_length(self):
         assert True == self.tree.calculate_hamming_distance('book', 'books')
 
@@ -45,9 +89,6 @@ class TestBKTree:
 
     def test_same_length_many(self):
         assert self.tree.calculate_hamming_distance('miracle', 'milagro') == 4
-
-    def test_calculate_height(self):
-        assert self.tree.calculate_height(self.built_tree) == 4
 
 
 class TestGraph:
